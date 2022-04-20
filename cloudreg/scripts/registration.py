a16446a1b6148a757fb204bfb076c2e994696675
hding9 <hding9@hawk.iit.edu>
2022-04-20 00:09:46 -0500

    Update docker run command with predownloaded file; Add a file existing check before downloads.

--- cloudreg/scripts/registration.py
+++ cloudreg/scripts/registration.py
@@ -151,11 +151,19 @@ def register(
     # download raw data at lowest 15 microns
     voxel_size = download_data(input_s3_path, target_name, 15000)
     print(f"[DEBUG] voxel_size = {voxel_size}.")
-    # download atlas and parcellations at registration resolution
-    _ = download_data(atlas_s3_path, atlas_name, registration_resolution, resample_isotropic=True)
-    _ = download_data(parcellation_s3_path, parcellation_name, registration_resolution, resample_isotropic=True)
-    # also download high resolution parcellations for final transformation
-    parcellation_voxel_size, parcellation_image_size = download_data(parcellation_s3_path, parcellation_hr_name, 10000, return_size=True)
+    if not os.path.exists(atlas_name):
+        # download atlas and parcellations at registration resolution
+        _ = download_data(atlas_s3_path, atlas_name, registration_resolution, resample_isotropic=True)
+    if not os.path.exists(parcellation_name):
+        _ = download_data(parcellation_s3_path, parcellation_name, registration_resolution, resample_isotropic=True)
+    # download high resolution in a whole always give me some connection failure
+    # thus break the numpy array into pieces and pre-download the file and combine
+    if not os.path.exists(parcellation_hr_name):
+        # also download high resolution parcellations for final transformation
+        parcellation_voxel_size, parcellation_image_size = download_data(parcellation_s3_path, parcellation_hr_name, 10000, return_size=True)
+    else:
+        parcellation_voxel_size = [10.0, 10.0, 10.0]
+        parcellation_image_size = [1320, 800, 1140]
 
     print("[DEBUG] Download Finished.")
 
